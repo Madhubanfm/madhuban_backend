4161a8df7f82693cb855ba3db51110d6602e8047
Aditya Nathu Shewale <er.adityashewale@gmail.com>
2026-05-09 23:57:22 +0530

    add

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,5 +1,4 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NodeHttpHandler } from "@smithy/node-http-handler";
 
 function requiredEnv(name: string): string {
@@ -82,16 +81,6 @@ export function buildPublicUrl(key: string) {
   return `${publicBaseUrl}/${encodedKey}`;
 }
 
-export async function getPresignedPutUrl(params: { key: string; contentType: string; expiresIn?: number }): Promise<string> {
-  const { bucket } = getS3Config();
-  const client = getS3Client();
-  const command = new PutObjectCommand({
-    Bucket: bucket,
-    Key: params.key,
-    ContentType: params.contentType
-  });
-  return getSignedUrl(client, command, { expiresIn: params.expiresIn ?? 300 });
-}
 
 export async function uploadBufferToS3(params: { key: string; contentType: string; body: Buffer }) {
   const { bucket } = getS3Config();
